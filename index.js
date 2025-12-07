8dd211eed2ee38f9ef1d2e5c7222cc45a02c6b28
Minhajur rahman <minhajurrahman2716@gmail.com>
2025-12-07 23:53:19 +0600

    Delete api created for book

--- index.js
+++ index.js
@@ -130,6 +130,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/delete-book/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await booksCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
